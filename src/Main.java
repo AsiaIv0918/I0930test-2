a3ece5e5269899b2790326b70fc2326f25e5272a
AsiaIv0918 <104051067@live.asia.edu.tw>
2017-09-30 14:25:52 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+//攝氏轉華式
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int F,C;
+        C= in.nextInt();
+        F=(C*9/5+32);
+        System.out.printf("%d\n",F);
+
+    }
+}
